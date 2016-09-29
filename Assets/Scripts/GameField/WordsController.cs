e3c0a28e509048e7ee976a33284a03c9e549115f
Andrii Vasylyk <deadsith@hotmail.com>
2016-09-29 18:43:04 +0300

    Started fixing #18

---
+++ Assets/Scripts/GameField/WordsController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class WordsController : MonoBehaviour
+{
+
+    public GameObject WordButton;
+    private GameObject parent;
+	void Start ()
+    {
+        parent = GameObject.FindWithTag("Words");
+        for (int i = 0; i < 10; i++)
+        {
+            AddButton(i.ToString());
+        }
+	}
+	
+	
+	void AddButton (string word)
+	{
+	    var button = Instantiate(WordButton, parent.transform) as GameObject;
+	    button.GetComponentInChildren<Text>().text = word;
+	}
+}
